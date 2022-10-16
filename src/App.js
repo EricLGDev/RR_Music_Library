247b377dc71d6f801cbe543bd5f0436a1dc3fa31
Eric <ericlg.link@gmail.com>
2022-10-16 09:40:44 -0700

    Refactored with Context

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
   let [search, setSearch] = useState('')
   let [message, setMessage] = useState('Search for Music!')
   let [data, setData] = useState([])
+  import { DataContext } from './Context/DataContext'
 
   const API_URL = 'https://itunes.apple.com/search?term='
 
@@ -35,7 +36,9 @@ const App = () => {
       <div>
           <SearchBar handleSearch={handleSearch} />
           {message}
-          <Gallery data={data} />
+          <DataContext.Provider value={data}>
+            <Gallery />
+          </DataContext.Provider>
       </div>
   )
 }
